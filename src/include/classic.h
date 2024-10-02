a37ee3d0766997b16a16fb4c820e419e7440948f
Robledo Pazotto <robledo@gmail.com>
2024-10-02 15:29:25 -0700

    .

---
+++ src/include/classic.h
@@ -0,0 +1,8 @@
+//
+// Created by robledo on 10/2/24.
+//
+
+#ifndef CLASSIC_H
+#define CLASSIC_H
+
+#endif //CLASSIC_H
---
+++ src/keyboard/classic.c
@@ -0,0 +1,5 @@
+//
+// Created by robledo on 10/2/24.
+//
+
+#include "classic.h"
